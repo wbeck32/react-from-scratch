cea95082a72ed804e139bb56fa98f5f35f94cb96
Your Name <you@example.com>
2019-10-10 20:28:43 -0700

    webeck@gmail.com

---
+++ src/components/PageContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+const PageContainer = () => {
+	const pageContainerStyle = {
+		height: '.25vh',
+		border: '2px solid pink'
+	}
+	return (<div style={pageContainerStyle}><Header /><Main /><Footer /></div>)
+
+
+
+}
+
+export default Footer
