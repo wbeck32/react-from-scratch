03d7b8b32489f3cff2a2ceef1aae334467f62cee
Wendy Beck <2320919+wbeck32@users.noreply.github.com>
2019-10-10 20:34:44 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const Header = () => {
+	const headerStyle = {
+		height: '.25vh',
+		border: '2px solid green'
+	}
+	return (<div style={headerStyle}>Hello! I am Header!</div>)
+
+
+
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const Main = () => {
+	const mainStyle = {
+		height: '.25vh',
+		border: '2px solid orange'
+	}
+	return (<div style={mainStyle}>Hello! I am Main!</div>)
+
+
+
+}
+
+export default Main
